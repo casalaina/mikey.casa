f0cfd05fe51a9683e6c1fbedcd94972b37b15d08
Mikey Casalaina <casalaina@gmail.com>
2021-11-14 19:30:18 +0100

    Adds banana experiment

--- .svelte-kit/dev/generated/manifest.js
+++ .svelte-kit/dev/generated/manifest.js
@@ -2,6 +2,7 @@ const c = [
 	() => import("../components/layout.svelte"),
 	() => import("../components/error.svelte"),
 	() => import("../../../src/routes/index.svelte"),
+	() => import("../../../src/routes/banana.svelte"),
 	() => import("../../../src/routes/laina.svelte")
 ];
 
@@ -11,8 +12,11 @@ export const routes = [
 	// src/routes/index.svelte
 	[/^\/$/, [c[0], c[2]], [c[1]]],
 
+	// src/routes/banana.svelte
+	[/^\/banana\/?$/, [c[0], c[3]], [c[1]]],
+
 	// src/routes/laina.svelte
-	[/^\/laina\/?$/, [c[0], c[3]], [c[1]]]
+	[/^\/laina\/?$/, [c[0], c[4]], [c[1]]]
 ];
 
 // we import the root layout/error components eagerly, so that
