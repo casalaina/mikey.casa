f22d7a3c91316f21307066bad75f87f9cd853663
Mikey Casalaina <casalaina@gmail.com>
2022-02-22 13:22:53 +0100

    Update gitignore

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script context="module">
+	// @ts-check
+	/**
+	 * @type { RouteLoad }
+	 */
+	export async function load() {
+		return {
+			status: 307,
+			redirect: '/laina'
+		};
+	}
+</script>
+
+<script>
+	import { goto } from '$app/navigation';
+	goto('/laina');
+</script>
---
+++ src/utils/GoogleAnalytics.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let page;
+	let id = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
+	if (typeof window !== 'undefined') {
+		window.dataLayer = window.dataLayer || [];
+		window.gtag = function gtag() {
+			window.dataLayer.push(arguments);
+		};
+		window.gtag('js', new Date());
+		window.gtag('config', id, { send_page_view: false });
+	}
+	$: {
+		if (typeof gtag !== 'undefined') {
+			window.gtag('config', id, {
+				page_path: $page.path
+			});
+		}
+	}
+</script>
+
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id={id}"></script>
+</svelte:head>
