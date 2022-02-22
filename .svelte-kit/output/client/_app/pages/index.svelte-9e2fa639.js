01bf8c832b492a9124e1228b7a93b6d4e1351a25
Mikey Casalaina <casalaina@gmail.com>
2022-02-22 13:22:33 +0100

    Update gitignore

--- .svelte-kit/output/client/_app/pages/index.svelte-9e2fa639.js
+++
@@ -1,1 +0,0 @@
-import{S as n,i as s,s as t}from"../chunks/vendor-4369daa9.js";import{r}from"../chunks/singletons-bb9012b7.js";const o=async function(n,s){return r.goto(n,s,[])};async function a(){return{status:307,redirect:"/laina"}}function e(n){return o("/laina"),[]}export default class extends n{constructor(n){super(),s(this,n,e,null,t,{})}}export{a as load};
