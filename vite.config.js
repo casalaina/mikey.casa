894ffb989bf32d7d4fc08ebb667e0fc5231260cb
Mikey Casalaina <casalaina@gmail.com>
2024-05-01 16:10:33 +0200

    chore: sveltkit version bump

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import path from 'path';
 
-/** @type {import('vite').UserConfig} */
-const config = {
-	plugins: [sveltekit()]
-};
-
-export default config;
+export default defineConfig({
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'~': path.resolve('./src')
+		},
+		extensions: ['.js', '.svelte', '.json']
+	}
+});
